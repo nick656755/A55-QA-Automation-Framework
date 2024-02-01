749dec7dbf6f176c1303d23ce2671befc9eb9b5c
nick656755 <157446224+nick656755@users.noreply.github.com>
2024-01-31 22:11:13 -0600

    Homework 18

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest{
+
+    @Test
+    public void playSong(){
+        //Step1: Open koel app and login
+        navigateToPage();
+        provideEmail("nicolai.luta@testpro.io");
+        providePassword("tAPO0uuR");
+        clickSubmit();
+        // Step2: click 'Play' and 'Next' btn
+        clickPlay();
+        // Step3: check if the song is playing, by checking if 'sound bar icon' is moving
+        Assert.assertTrue(isSongPlaying());
+    }
+    public void  clickPlay() {
+        WebElement playNextBtn = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
+        WebElement playBtn = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
+        playNextBtn.click();
+        playBtn.click();
+    }
+    public boolean isSongPlaying(){
+        WebElement soundBar = driver.findElement(By.xpath("//div[@data-testid='sound-bar-play']"));
+        return soundBar.isDisplayed();
+    }
+}
