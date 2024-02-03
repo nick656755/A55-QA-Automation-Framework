ae4097c39be49ac6a3793eae10a8a88aafbb0d27
nick656755 <157446224+nick656755@users.noreply.github.com>
2024-02-03 14:35:35 -0600

    Homework 19

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest{
+
+  @Test
+    public void deletePlaylist() throws InterruptedException{
+      String expectedPlaylistDeletedMsg = "Deleted playlist \"test.\"";
+      //Step1: navigate to koel and login
+      navigateToPage();
+      provideEmail("nicolai.luta@testpro.io");
+      providePassword("tAPO0uuR");
+      clickSubmit();
+
+      //Step2: open playlist
+      openPlaylist();
+
+      //Step3: click delete btn
+    clickDeleteBtn();
+    Thread.sleep(4000);
+
+    //Step4: check if playlist is deleted
+    Assert.assertEquals(getDeletedPlaylistMsg(),expectedPlaylistDeletedMsg);
+  }
+
+  public void clickDeleteBtn() {
+    WebElement deletePlaylist = driver.findElement(By.cssSelector(".btn-delete-playlist"));
+    deletePlaylist.click();
+  }
+
+  public void openPlaylist() {
+        WebElement playlist = driver.findElement(By.cssSelector("#playlists > ul > li:nth-child(5)"));
+        playlist.click();
+    }
+    public String getDeletedPlaylistMsg(){
+    WebElement notificationMsg = driver.findElement(By.cssSelector("div.success.show"));
+    return notificationMsg.getText();
+    }
+
+
+}
