f97f7c12bb65ff510d959257fe240a0e080bcc2c
nick656755 <157446224+nick656755@users.noreply.github.com>
2024-02-15 20:07:32 -0600

    Homework24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,7 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -13,6 +18,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 import java.util.UUID;
 
@@ -24,15 +31,16 @@ public class BaseTest {
     @BeforeSuite
     void setupClass() {
         WebDriverManager.chromedriver().setup();
+
     }
+
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public void launchBrowser(String baseURL) {
+    public void launchBrowser(String baseURL) throws MalformedURLException {
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
 
-        driver = new ChromeDriver(options);
+        driver = pickBrowser(System.getProperty("browser"));
+
         // implicit wait
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //explicit wait
@@ -42,67 +50,65 @@ public class BaseTest {
         navigateToPage(baseURL);
         actions =new Actions(driver);
     }
-    @AfterMethod
-    public void closeBrowser() {
-        driver.quit();
+
+    public WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.1.8:4444/";
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "MicrosoftEdge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                return driver = new EdgeDriver(edgeOptions);
+            case "grid-edge":
+                caps.setCapability("browserName", "MicrosoftEdge");
+                return driver= new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                return driver= new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver= new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                return driver= new ChromeDriver(chromeOptions);
+
+        }
     }
 
+        @AfterMethod
+        public void closeBrowser () {
+            driver.quit();
+        }
 
-    public void navigateToPage(String url) {
-        driver.get(url);
-    }
 
-    public void provideEmail(String email) {
-        //WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
-        emailField.clear();
-        emailField.sendKeys(email);
-    }
+        public void navigateToPage (String url){
+            driver.get(url);
+        }
 
-    public void providePassword(String password) {
-        //WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-        WebElement passwordField =wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
-        passwordField.clear();
-        passwordField.sendKeys(password);
-    }
+        public void provideEmail (String email){
+            //WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+            WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
+            emailField.clear();
+            emailField.sendKeys(email);
+        }
 
-    public void clickSubmit() {
-        //WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
-        WebElement submit =wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
-        submit.click();
-    }
-    /*public void isAvatarDisplayed() {
-        //WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
-        WebElement avatarIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[class='avatar']")));
-        Assert.assertTrue(avatarIcon.isDisplayed());
-    }
+        public void providePassword (String password){
+            //WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+            WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
+            passwordField.clear();
+            passwordField.sendKeys(password);
+        }
 
-    public void clickSaveButton() {
-        //WebElement saveButton = driver.findElement(By.cssSelector("button.btn-submit"));
-        WebElement saveButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.btn-submit")));
-        saveButton.click();
-    }
+        public void clickSubmit () {
+            //WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+            WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
+            submit.click();
+        }
 
-    public void provideProfileName(String randomName) {
-       // WebElement profileName = driver.findElement(By.cssSelector("[name='name']"));
-        WebElement profileName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
-        profileName.clear();
-        profileName.sendKeys(randomName);
     }
-
-    public void provideCurrentPassword(String password) {
-        //WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
-        WebElement currentPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='current_password']")));
-        currentPassword.clear();
-        currentPassword.sendKeys(password);
-    }
-
-    public String generateRandomName() {
-        return UUID.randomUUID().toString().replace("-", "");
-    }
-
-    public void clickAvatarIcon() {
-        WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
-        avatarIcon.click();
-    }*/
-}
